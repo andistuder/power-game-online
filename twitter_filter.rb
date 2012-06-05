f1315df6371824e7354c6af420859b824f2a1186
andistuder <andi@cenatus.org>
2012-06-05 01:14:20 +0100

    updated twitter filter

--- twitter_filter.rb
+++ twitter_filter.rb
@@ -4,12 +4,24 @@ require File.join(File.dirname(__FILE__), 'tweet_store')
 load File.join(File.dirname(__FILE__), 'twitter_authentication.rb')
 
 @tweet_store = TweetStore.new
-query_params = {}
-query_params.merge!(:track => 'powergameonline')
-query_params.merge!(:follow => 'PGOtest')
-puts query_params
-#TweetStream::Client.new.track('powergameonline') do |status|
-TweetStream::Client.new.filter(query_params ) do |status|
+#query_params = {}
+#query_params.merge!(:track => %w(powergameonline power))
+#query_params.merge!(:follow => %w(PGOtest))
+#puts query_params
+#TweetStream::Client.new
+#@client = TweetStream::Client.new
+
+#@client.on_inited do
+#  puts 'Connected...'
+#end
+#puts "someting"
+#@client.on_no_data_received do
+#  puts 'Make note of no data, possi'
+#end
+#@client.sample do |status|
+#  puts "[#{status.user.screen_name}] #{status.text}"
+#end
+TweetStream::Client.new.track('power', 'powergameonline') do |status|
   @tweet_store.push(
     'id' => status[:id],
     'text' => status.text,
@@ -21,3 +33,5 @@ TweetStream::Client.new.filter(query_params ) do |status|
     'received_at' => Time.new.to_i
   )
 end
+#end
+#puts "over"
